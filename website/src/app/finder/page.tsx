50efd90c385cd3b5799ed57cabd11684416b9e3f
Gabriel Garcia <80603461+ggarcia453@users.noreply.github.com>
2025-02-09 14:06:47 -0800

    Changes to finderpage

--- website/src/app/finder/page.tsx
+++ website/src/app/finder/page.tsx
@@ -1,18 +1,16 @@
 "use client"
 import { useState, useEffect } from 'react';
 import { fetchData } from "../functions/apicall";
+import { Category } from '../types/category';
 const FinderPage = () => {
-    const [categories, setCategories] = useState<string[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
     useEffect(() => {
         const loadInitialData = async () => {
           try {
-            const results = await fetchData("card", "", "");
-            const sortedResults = [...results].sort((a, b) => a.id - b.id);
-            console.log(sortedResults);
+            const results = await fetchData("category", "", "");
+            setCategories(results)
           } catch (err) {
             console.error(err);
-          } finally {
-            
           }
         };
     
@@ -20,6 +18,20 @@ const FinderPage = () => {
       }, []);
     return <main>
         <p className="text-center text-xl"> Under Construction </p>
+        <table className="border-collapse border">
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map((cat) => (
+              <tr key={cat.id}>
+                <td className="border text-center p-2">{cat.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </main>
 }
 export default FinderPage;
